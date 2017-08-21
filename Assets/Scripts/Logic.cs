4a4ecd038a0400b1145a486445c0e761b705ed7d
Volo <volo1992@gmail.com>
2017-08-21 11:52:34 +0200

    Add a little UI for the score

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Logic : MonoBehaviour {
 
     [SerializeField]
     private int score;
     private int cubeScore;
+    [SerializeField]
+    private Text scoreText;
 
     private void Awake()
     {
         score = 0;
         cubeScore = 0;
+        if (Application.loadedLevelName.Equals("Level_1"))
+        {
+            scoreText.text = "0 / 15";
+        } else if (Application.loadedLevelName.Equals("Level_2"))
+        {
+            scoreText.text = "0 / 405";
+        } else
+        {
+            scoreText.text = "0 / 3";
+        }
+
     }
 
     // Use this for initialization
@@ -29,6 +43,7 @@ public class Logic : MonoBehaviour {
         if (other.gameObject.CompareTag("Goal"))
         {
             score++;
+            scoreText.text = score.ToString() + " / 15";
             Debug.Log(score);
             if (score >= 15)
             {
@@ -39,7 +54,8 @@ public class Logic : MonoBehaviour {
         if (other.gameObject.CompareTag("Cube"))
         {
             cubeScore++;
-            if(cubeScore%27 == 0)
+            scoreText.text = cubeScore.ToString() + " / 405";
+            if (cubeScore%27 == 0)
             {
                 score++;
                 Debug.Log(score);
@@ -53,10 +69,11 @@ public class Logic : MonoBehaviour {
         if (other.gameObject.CompareTag("HardGoal"))
         {
             score++;
+            scoreText.text = score.ToString() + " / 3";
             Debug.Log(score);
             if (score >= 3)
             {
-                Application.LoadLevel("Level_1");
+                Application.Quit();
             }
         }
     }    
